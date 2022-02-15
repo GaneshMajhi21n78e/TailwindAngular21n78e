0a7e062e9ac68032fca611599f0a130f910379ca
ganeshmajhi21n78e <ganesh.majhi@21n78e.com>
2022-02-15 17:19:05 +0530

    added features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FeaturesComponent } from './Components/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
