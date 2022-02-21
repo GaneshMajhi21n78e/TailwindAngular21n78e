b1e9c53f52d2fd518653a163a510e0dcf8b25913
manan427 <modimanan00@gmail.com>
2022-02-21 17:10:00 +0530

    added routing

---
+++ src/app/Pages/casestudies-page/casestudies-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-casestudies-page',
+  templateUrl: './casestudies-page.component.html',
+  styleUrls: ['./casestudies-page.component.css']
+})
+export class CasestudiesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/Pages/joinus-page/joinus-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joinus-page',
+  templateUrl: './joinus-page.component.html',
+  styleUrls: ['./joinus-page.component.css']
+})
+export class JoinusPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
